476954c5ac01e7654a4ab5f2e0ae79b61cc8fabb
nachkonvention <j.c.lohmueller@gmail.com>
2017-03-10 13:28:32 +0100

    implemented basic db connection fo get and post

--- main.go
+++ main.go
@@ -2,32 +2,52 @@ package main
 
 import (
 	"encoding/json"
-
-	"time"
+	"log"
 
 	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/go-martini/martini"
 	"github.com/nachkonvention/go-schedule/model"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
-
-var appointments []model.Appointment = []model.Appointment{
-	model.NewAppointment("foo", "bar", time.Now(), time.Now(), model.Talk),
-	model.NewAppointment("bar", "foo", time.Now(), time.Now(), model.Talk),
-}
 
 func main() {
 	m := martini.Classic()
 
 	m.Get("/api/appointments", func() string {
+		session := getDatabaseSession()
+		defer session.Close()
+
+		var appointments []model.Appointment
+		session.DB("go-schedule").C("appointments").Find(bson.M{}).All(&appointments)
+
 		appointment, _ := json.Marshal(appointments)
 		return string(appointment)
 	})
 
 	m.Post("/api/appointments", binding.Bind(model.Appointment{}), func(appointment model.Appointment) string {
-		appointments = append(appointments, appointment)
+		session := getDatabaseSession()
+		defer session.Close()
+
+		appointmentsCollection := session.DB("go-schedule").C("appointments")
+
+		err := appointmentsCollection.Insert(appointment)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		result, _ := json.Marshal(appointment)
 		return string(result)
 	})
 
 	m.Run()
+}
+
+func getDatabaseSession() *mgo.Session {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	return session
 }
--- model/appointment.go
+++ model/appointment.go
@@ -5,13 +5,15 @@ import (
 )
 
 // Appointment represents an appointment
-type Appointment struct {
-	Title           string
-	Starttime       time.Time
-	Endtime         time.Time
-	Description     string
-	AppointmentType AppointmentType
-}
+type (
+	Appointment struct {
+		Title           string
+		Starttime       time.Time
+		Endtime         time.Time
+		Description     string
+		AppointmentType AppointmentType
+	}
+)
 
 // NewAppointment creates a new appointment
 func NewAppointment(title, description string, starttime, endtime time.Time, appointmentType AppointmentType) (appointment Appointment) {
